95b84c79458fa49e948f562ea33c753ee8d30558
TheBlueGenie <thebluegenieuk@gmail.com>
2024-01-31 13:01:02 +0000

    Initial Commit

---
+++ lua/core/options.lua
@@ -0,0 +1,22 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.opt.backspace = '2'
+vim.opt.showcmd = true
+vim.opt.laststatus = 4
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.autoread = true
+
+-- use spaces for tabs and whatnot
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.termguicolors = true
+
+--Line numbers
+vim.wo.number = true
+vim.wo.relativenumber = true
