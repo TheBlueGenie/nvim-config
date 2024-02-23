be45d25cf5927fd20251d291f9e8aacb7a345aa3
TheBlueGenie <thebluegenieuk@gmail.com>
2024-02-23 16:30:56 +0000

    add Odin Language Server

--- lua/core/plugin_config/lsp_config.lua
+++ lua/core/plugin_config/lsp_config.lua
@@ -1,5 +1,5 @@
 require("mason-lspconfig").setup({
-  ensure_installed = { "lua_ls", "rust_analyzer"}
+  ensure_installed = { "lua_ls", "rust_analyzer", "ols"}
 })
 
 local lspconfig = require('lspconfig')
@@ -31,7 +31,7 @@ require("lspconfig").lua_ls.setup {
 --require("lspconfig").solargraph.setup({})
 --require("lspconfig").tsserver.setup({})
 --require("lspconfig").gopls.setup({})
---require("lspconfig").tailwindcss.setup({})
+require("lspconfig").ols.setup({})
 require("lspconfig").rust_analyzer.setup({})
 
 vim.api.nvim_create_autocmd('LspAttach', {
